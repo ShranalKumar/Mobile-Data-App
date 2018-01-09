e9b05eb8bbd687c6b279af4da9706b7878282a60
Shranal Kumar <skmu316@aucklanduni.ac.nz>
2018-01-09 16:55:38 +1300

    Started Dashboard Design

---
+++ MobileApp/MobileApp/MobileApp.Android/Views/AdminDashboardView.cs
@@ -0,0 +1,18 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace MobileApp.Droid.Views
+{
+    [Activity(Label = "MobileApp", MainLauncher = true, Icon = "@mipmap/icon")]
+    public class AdminDashboardView : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            
+            SetContentView(Resource.Layout.AdminDashboardLayout);
+        }
+    }
+}
+
