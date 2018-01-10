00fe1d6d4432d5cf42e49e0d89a559676deac92a
Shranal Kumar <skmu316@aucklanduni.ac.nz>
2018-01-10 15:30:08 +1300

    Made some updates to design of AdminDashboardLayout.axml - Shranal

--- MobileApp/MobileApp/MobileApp.Android/Views/AdminDashboardView.cs
+++ MobileApp/MobileApp/MobileApp.Android/Views/AdminDashboardView.cs
@@ -4,14 +4,35 @@ using Android.OS;
 
 namespace MobileApp.Droid.Views
 {
-    [Activity(Label = "MobileApp", MainLauncher = true, Icon = "@mipmap/icon")]
+    [Activity(Theme = "@style/MainTheme", MainLauncher = true)]
     public class AdminDashboardView : Activity
     {
+        private ImageButton _hamburgerIcon;
+        private ImageButton _notificationButton;
+        private ImageButton _accountSwitcher;
+        private ImageView _mobileIcon;
+
+        private TextView _productName;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             
             SetContentView(Resource.Layout.AdminDashboardLayout);
+
+            _hamburgerIcon = FindViewById<ImageButton>(Resource.Id.MenuButton);
+            _mobileIcon = FindViewById<ImageView>(Resource.Id.MobileIcon);
+            _notificationButton = FindViewById<ImageButton>(Resource.Id.NotificationButton);
+            _accountSwitcher = FindViewById<ImageButton>(Resource.Id.AccountSwitcher);
+            _productName = FindViewById<TextView>(Resource.Id.ProductName);
+
+            _hamburgerIcon.SetImageResource(Resource.Drawable.Menu);
+            _notificationButton.SetImageResource(Resource.Drawable.NotificationIcon);
+            _accountSwitcher.SetImageResource(Resource.Drawable.ChevronDownIcon);
+            _mobileIcon.SetImageResource(Resource.Drawable.MobileIcon);
+            //_productName.Text = "Mobile";
+
+
         }
     }
 }
