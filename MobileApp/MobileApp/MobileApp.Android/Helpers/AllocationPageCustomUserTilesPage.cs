f94b321e12d6132194c51e96990460581f3e03ab
Soumik Patel <patel.soumik4@hotmail.com>
2018-02-06 16:17:36 +1300

    Working on Allocation Page

--- MobileApp/MobileApp/MobileApp.Android/Helpers/AllocationPageCustomUserTilesPage.cs
+++ MobileApp/MobileApp/MobileApp.Android/Helpers/AllocationPageCustomUserTilesPage.cs
@@ -20,11 +20,15 @@ namespace MobileApp.Droid.Helpers
 	public partial class AllocationPageCustomUserTilesPage : ContentView 
 	{
 		public static List<LinearLayout> UserTiles;
+        public static double totalAllocated = 0;
+        public static double progressChanged;
 
-		public static void getTiles(Android.Widget.LinearLayout parent)
+
+        public static void getTiles(Android.Widget.LinearLayout parent)
 		{
 			UserTiles = new List<LinearLayout>();
 			int pixelDensity = (int)Android.Content.Res.Resources.System.DisplayMetrics.Density;
+            
 			foreach (User user in Controller._users)
 			{
 				
@@ -46,17 +50,21 @@ namespace MobileApp.Droid.Helpers
 
                 ContextThemeWrapper userAllocatedSliderContext = new ContextThemeWrapper(UserDetailsTextLayout.Context, Resource.Style.UserAllocationSliderStyle);
                 SeekBar userAllocationSlider = new SeekBar(userAllocatedSliderContext, null, Resource.Style.UserAllocationSliderStyle);
-                double progress = (user.Used / user.Allocated) * 100;
+                double progress = (user.Used / Controller._planDataPool) * 100;
                 userAllocationSlider.Progress = (int)(progress);
                 userAllocationSlider.ProgressChanged += (object sender, SeekBar.ProgressChangedEventArgs e) =>
                 {
                     if (e.FromUser)
                     {
-                        double progressChanged = ((double)e.Progress / 100) * (Controller._totalUnAllocated + user.Allocated);
-                        double dif = progressChanged - user.Allocated;
-                        AllocationPageView._remainingDataAmount.Text = String.Format(StringConstants.Localizable.DataAmount, Math.Round(Controller._totalUnAllocated - dif, 1));
+                        double progressChanged = ((double)e.Progress / 100) * (Controller._planDataPool);
+                        Allocated.Text = string.Format(StringConstants.Localizable.DataAmount, Math.Round(progressChanged, 1));
+                        user.Allocated = progressChanged;
                     }
+
                 };
+
+                //AllocationPageView._remainingDataAmount.Text = String.Format(StringConstants.Localizable.DataAmount, Math.Round(Controller._totalUnAllocated - dif, 1));
+
 
                 parent.AddView(User);                
 				User.AddView(UserDetailsTextLayout);
--- MobileApp/MobileApp/MobileApp.Android/Views/AllocationPageView.cs
+++ MobileApp/MobileApp/MobileApp.Android/Views/AllocationPageView.cs
@@ -26,7 +26,7 @@ namespace MobileApp.Droid.Views
         private TextView _remainingDataText;
         public static TextView _remainingDataAmount;
         private TextView _weeklyModeText;
-        private Button _saveButon;
+        private Button _saveButton;
         private RadialProgressView _radialProgress;
 		private ScrollView _allocationUserScrollView;
 		private List<LinearLayout> _allocationUserTileList;
@@ -48,12 +48,17 @@ namespace MobileApp.Droid.Views
 			var pixelToDp = (int)Android.Content.Res.Resources.System.DisplayMetrics.Density;
 
             //Radial Progress            
-            _radialProgress.LayoutParameters.Height = 100 * pixelToDp;
-            _radialProgress.LayoutParameters.Width = 100 * pixelToDp;
-            _radialProgress.LabelHidden = true;
+            //_radialProgress.LayoutParameters.Height = 100 * pixelToDp;
+            //_radialProgress.LayoutParameters.Width = 100 * pixelToDp;
+            //_radialProgress.LabelHidden = true;
 
             _backButton.SetImageResource(Resource.Drawable.ArrowBackIcon);
 			_backButton.Click += delegate { StartActivity(typeof(AdminDashboardView)); };
+
+            _saveButton.Click += delegate {
+                Controller._users.ForEach(x => Controller._totalAllocated += x.Allocated);
+                Console.WriteLine(Controller._totalAllocated.ToString());
+            };
         }
 
         protected void findAllElements()
@@ -64,9 +69,9 @@ namespace MobileApp.Droid.Views
             _currentPlanDataAmount = FindViewById<TextView>(Resource.Id.CurrentPlanDataAmount);
             _remainingDataText = FindViewById<TextView>(Resource.Id.RemainingDataText);
             _remainingDataAmount = FindViewById<TextView>(Resource.Id.RemainingDataAmount);
-            _radialProgress = FindViewById<RadialProgressView>(Resource.Id.RadialProgressCircle);
-            _weeklyModeText = FindViewById<TextView>(Resource.Id.WeeklyModeText);
-            _saveButon = FindViewById<Button>(Resource.Id.SaveButton);
+            //_radialProgress = FindViewById<RadialProgressView>(Resource.Id.RadialProgressCircle);
+            //_weeklyModeText = FindViewById<TextView>(Resource.Id.WeeklyModeText);
+            _saveButton = FindViewById<Button>(Resource.Id.SaveButton);
 			_allocationUserScrollView = FindViewById<ScrollView>(Resource.Id.ScrollableLayout);
 			_userDataAllocationListItem = FindViewById<LinearLayout>(Resource.Id.UserDataAllocationList);
 
@@ -79,8 +84,8 @@ namespace MobileApp.Droid.Views
             _currentPlanDataAmount.Text = String.Format(StringConstants.Localizable.DataAmount, Controller._planDataPool);
             _remainingDataText.Text = StringConstants.Localizable.UnAllocatedData;
             _remainingDataAmount.Text = string.Format(StringConstants.Localizable.DataAmount, Math.Round(Controller._totalUnAllocated, 2));
-            _weeklyModeText.Text = StringConstants.Localizable.WeeklyMode;
-            _saveButon.Text = StringConstants.Localizable.SaveButton;
+            //_weeklyModeText.Text = StringConstants.Localizable.WeeklyMode;
+            _saveButton.Text = StringConstants.Localizable.SaveButton;
         }
     }
 }
